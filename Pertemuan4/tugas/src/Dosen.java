1449bb3d1f308e07f4494044dd9c52e1cef24fb3
khanzaqaila <khanzaqaila857@gmail.com>
2026-03-16 08:50:56 +0700

    /*  Nama File   : Pegawai.java  *  Deskripsi   : berisi atribut dan method dalam class Pegawai  *  Pembuat     : Khanza Qaila/24060124120011  *  Tanggal     : 9 Maret 2026 */
     git commit  -m "update"

--- Pertemuan4/tugas/src/Dosen.java
+++ Pertemuan4/tugas/src/Dosen.java
@@ -1,5 +1,5 @@
 /*  Nama File   : Dosen.java
- *  Deskripsi   : berisi atribut dan method dalam class Dosen
+ *  Deskripsi   : berisi atribut dan method dalam class Dosen 
  *  Pembuat     : Khanza Qaila/24060124120011
  *  Tanggal     : 9 Maret 2026 */
 
