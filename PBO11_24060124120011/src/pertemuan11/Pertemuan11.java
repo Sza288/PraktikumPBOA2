bb37c33e10a38cf607cefd134bc47fd222683a86
Sza <khanzaqaila857@gmail.com>
2026-06-01 12:29:11 +0700

    done pert11 modularisasi A

---
+++ PBO11_24060124120011/src/pertemuan11/Pertemuan11.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan11;
+
+/**
+ *
+ * @author lowli
+ */
+public class Pertemuan11 {
+    public static void main(String[] args) {
+        Gui form = new Gui();
+        form.setVisible(true);
+    }
+    
+}
